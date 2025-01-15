f697e84f17ae137f4fd7228cf4879e0d8ad96c2d
soniprem831 <premsoni8838@gmail.com>
2025-01-15 16:09:35 +0530

    Integrated Multer for file handling in the backend

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import {register,login,updateProfile,logout} from "../controllers/user.controller.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
+import { singleUpload } from "../middleware/multer.js";
 
 
 const router=express.Router();
 
-router.route("/register").post(register);
+router.route("/register").post(singleUpload,register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated,updateProfile);
